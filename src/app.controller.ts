e7ed543bf41f6cec75da773eddb5606822aec54f
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2026-07-23 22:31:43 -0500

    feat: login con roles, CORS y dashboard protegido con localStorage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  login(@Body() body:{ usuario: string; password: string }){
+    if (body.usuario === 'admin' && body.password === '1234') {
+      return { ok: true, rol: ' admin'};
+    }
+    if (body.usuario === 'profesor' && body.password === '1234') {
+      return { ok: true, rol: 'profesor'};
+    }
+    return { ok: false };
+  }
+
+  @Get('impresiones')
+  getImpresiones(){
+    return [
+      {profesor: 'Ana Torres', seccion:'1 A', hora:'10:00', descripcion:'10 copias, doble cara'},
+      {profesor: 'Luis Ramos', seccion:'2 B', hora:'11:30', descripcion:'Imprimir examen, 30 hojas'},
+    ]
   }
 }
