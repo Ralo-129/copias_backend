c4b6eb43b1fa960ee0b4bde9eb383b394b1ff713
Ralo-129 <rafaelmatador129@gmail.com>
2026-07-24 15:48:48 -0500

    Feature/login dashboard roles (#1)
    * feat: login con roles, CORS y dashboard protegido con localStorage
    
    * feat: endpoint de subida de archivos con FileInterceptor
    
    ---------
    
    Co-authored-by: Rafael-129 <rafael.garcia.c@tecsup.edu.pe>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { uptime } from 'process';
 
 @Controller()
 export class AppController {
@@ -9,4 +11,37 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('login')
+  login(@Body() body:{ usuario: string; password: string }){
+    if (body.usuario === 'admin' && body.password === '1234') {
+      return { ok: true, rol: 'admin'};
+    }
+    if (body.usuario === 'profesor' && body.password === '1234') {
+      return { ok: true, rol: 'profesor'};
+    }
+    return { ok: false };
+  }
+
+  @Get('impresiones')
+  getImpresiones(){
+    return [
+      {profesor: 'Ana Torres', seccion:'1 A', hora:'10:00', descripcion:'10 copias, doble cara'},
+      {profesor: 'Luis Ramos', seccion:'2 B', hora:'11:30', descripcion:'Imprimir examen, 30 hojas'},
+    ]
+  }
+
+  @Post('subir')
+  @UseInterceptors(FileInterceptor('archivo'))
+  subirArchivo(
+    @UploadedFile() archivo: Express.Multer.File,
+    @Body() body: { seccion: string; descripcion: string },
+  ){
+    console.log('Archivo recibido:', archivo?.originalname);
+    console.log('Seccion: ', body.seccion);
+    console.log('Descripcion: ', body.descripcion);
+
+    return {ok: true, mensaje: 'Archivo recibido correctamente'}
+  }
+
 }
