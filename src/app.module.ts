a855f7b65d70d29bf128df208222ffb578e48469
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2026-07-24 16:47:09 -0500

    feat: conexión a MongoDB Atlas, schema de Impresion y guardado real en /subir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Impresion, ImpresionSchema } from './impresiones/impresiones.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+    MongooseModule.forFeature([{ name: Impresion.name, schema: ImpresionSchema }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
